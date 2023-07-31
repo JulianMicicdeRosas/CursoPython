ae05885fcf272ebb6037b973db0bd4a933e81771
Reixon <reixon@gmail.com>
2023-07-31 20:43:27 -0300

    Mi version para ver que pasa

---
+++ Python3/practice-exercises/section-8-strings/7 - Julian.py
@@ -0,0 +1,10 @@
+"""
+Escribe un programa que convierta el primer carácter en mayúsculas y 
+los restantes 
+en minúsculas de un string introducido por teclado.
+No usar función 'capitalize'
+"""
+
+PonerUnaPalabraLoca = input()
+ 
+ 
