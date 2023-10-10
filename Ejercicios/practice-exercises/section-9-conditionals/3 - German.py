401f11f0d3937a8416de29d3258c650fddb613cf
GSartori <47071193+german108@users.noreply.github.com>
2023-10-09 22:35:47 -0300

    ultima actualizacion del ejercicio 2 y 3 de la seccion 9

---
+++ Ejercicios/practice-exercises/section-9-conditionals/3 - German.py
@@ -0,0 +1,48 @@
+"""
+Vamos a crear un programa, donde introducimos un número ENTERO y 
+debemos de decir si es POSITIVO, NEGATIVO ó NEUTRO (igual a 0).
+Datos de prueba:
+Introduce el número: -373
+Resultado esperado: -373 es NEGATIVO
+
+Introduce el número: 6
+Resultado esperado: 6 es POSITIVO.
+
+"""
+
+
+positivoNegativo = int(input("ingrese un entero: "))
+
+if(positivoNegativo >= 1):
+
+    print("es positivo, pues")
+
+elif(positivoNegativo <= -1):
+    print("es negativo papu")
+
+else:
+    print("te dio 0")
+
+    
+
+
+
+
+
+
+
+
+
+
+
+"""
+number_val = int(input("Introduce el número: \n"))
+
+
+if (number_val < 0): result = "NEGATIVO"
+elif (number_val > 0): result = "POSITIVO"
+else: result = "NEUTRO"
+
+print(f"{number_val} es {result}")
+
+"""
