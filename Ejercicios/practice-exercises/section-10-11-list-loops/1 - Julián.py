2240f84ced8a3e68cdec7def85ab2e1a3ff4e598
Reixon <reixon@gmail.com>
2023-12-26 20:28:10 -0300

    Cositas 26/12

---
+++ Ejercicios/practice-exercises/section-10-11-list-loops/1 - Julián.py
@@ -0,0 +1,20 @@
+"""
+Realizar un programa que imprima en pantalla los números del 1 al 100 (incluido).
+Lo haremos con las diferentes opciones
+* while
+* for (usando “range”)
+"""
+
+#while
+
+""""contador = 0
+
+while contador <= 100:
+    print (contador)
+    contador += 1
+"""
+#for
+
+for i in range(1, 100):
+    print (i)
+
---
+++ Ejercicios/practice-exercises/section-10-11-list-loops/2 - Julián.py
@@ -0,0 +1,19 @@
+"""
+Realizar un programa que imprima en pantalla los números impares del 1 al 100.
+Lo haremos con las diferentes opciones
+* while
+* for (usando “range”) y sin usar un condicional, solo con el uso del rango
+"""
+
+# while
+"""numeros = 1
+
+while numeros <= 100:
+    print (numeros)
+    numeros += 2
+
+"""
+
+for i in range(1,101,2):
+    print(i)
+    
